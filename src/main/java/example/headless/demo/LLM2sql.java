bb559564475b65ebcadf8578417ad3e3a95d5ac4
6387 <bianqc@sciyon.com>
2025-11-27 11:14:58 +0800

    jiemi

--- src/main/java/example/headless/demo/LLM2sql.java
+++ src/main/java/example/headless/demo/LLM2sql.java
@@ -19,10 +19,7 @@ import java.util.Map;
 
 public class LLM2sql {
 
-//    @Autowired
-//    public static Config config;
     static Config config;
-
     static {
         config = new Config();
         config.setApi_key(" ");
@@ -30,37 +27,31 @@ public class LLM2sql {
         config.setModel_name("gpt-4o-mini");
     }
     static MatchKeyword matchKeyword;
-    static String query = "近15天大数据分析部访问次数汇总";
+//    static String query = "近15天大数据分析部访问次数汇总";
+    static String query = "昨天大数据分析部访问次数汇总";
 
     public static Map<String, String> get_keyword(String text){
-        // 匹配Mysql数据库中的关键词
+        // 1、匹配查询问题中的指标（专有名称）
         Map<String, String> keyword_result = matchKeyword.matchword(text);
         System.out.println("-----关键词-------");
         System.out.println(keyword_result);
-
         List<String> metrics = formatMetricMap(keyword_result);
         metrics.add(" AGGREGATE 'SUM'");
+        // 2、补全变量补充Prompt提示模板中的4个占位符中的内容
         Example current_chat = Example.of(
-//                "近15天超音数访问次数汇总",
                 query,
                 "数字中心平台",
-//                "<访问次数 COMMENT '一段时间内用户的访问次数'>",
                 String.join("", metrics),
                 "<数据日期>",  //示例：Dimensions=[<部门>,<数据日期>]
                 "<部门='大数据分析部'>",  // values 暂时为空,为值字段; 示例：Values[<用户='jackjchen'>,<用户='robinlee'>]
                 ""
         );
-
+        // 3、提示词补充 + few_shot示例 + 当前问题 + 数据库中表字段信息
         String finalPrompt = PromptLoader.buildFewShotPrompt("Generate_sql_prompt", current_chat);
-
         System.out.println("-----模拟最终提示词-------");
         System.out.println(finalPrompt);
-        System.out.println("-----调用大模型生成SQL语句-------");
-/*        // 或者从配置文件/环境变量读取
-        Config config = new Config();
-        config.setApi_key(" ");
-        config.setUrl("http://langchain4j.dev/demo/openai/v1/");
-        config.setModel_name("gpt-4o-mini");*/
+
+        // 4、调用大模型生成语义SQL语句
         OpenAIClient client = OpenAIOkHttpClient.builder()
                 .apiKey(config.getApi_key())
                 .baseUrl(config.getUrl())
@@ -73,8 +64,7 @@ public class LLM2sql {
 
         try {
             ChatCompletion chatCompletion = client.chat().completions().create(params);
-            System.out.println(chatCompletion);
-            System.out.println("--------下面是返回的语义SQL---------");
+            System.out.println("--------调用LLM生成的语义SQL---------");
             String content = String.valueOf(chatCompletion.choices().get(0).message().content());
             System.out.println(content);
 
@@ -87,17 +77,12 @@ public class LLM2sql {
 
     public static List<String> formatMetricMap(Map<String, String> metricMap) {
         List<String> result = new ArrayList<>();
-
         for (Map.Entry<String, String> entry : metricMap.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue() != null ? entry.getValue() : ""; // 防止 value 为 null
-
-            // 拼接成 <key COMMENT 'value'> 格式
-            // 注意：如果 value 中包含单引号，需要转义（可选，根据使用场景）
             String formatted = "<" + key + " COMMENT '" + value + "'>";
             result.add(formatted);
         }
-
         return result;
     }
 
